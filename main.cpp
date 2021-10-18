1b9e9062783b4f133313be7a1a668b01be039edc
xTheCoderx <justaguyoney@gmail.com>
2021-10-18 15:58:58 +1300

    Added ToLower() function, which basically converts the string to a lowercase variant (wow, I bet you didn't know). More features will be added in the next commits.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+std::string ToLower(std::string &str) {
+    for (char &ch : str) {
+        ch = std::tolower(ch, std::locale());
+    }
+    return str;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "-- Welcome to everything I know in C++! --" << "\n";
+
+    std::string input;
+
+    std::cout << "Are you ready to start the adventure?" << "\n";
+
     return 0;
 }
