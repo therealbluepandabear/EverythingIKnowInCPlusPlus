3097c5dae82d7f071fc6c9a8e50e589cd8d69ed8
xTheCoderx <justaguyoney@gmail.com>
2021-10-18 17:48:36 +1300

    Updated adventure.

--- main.cpp
+++ main.cpp
@@ -47,7 +47,7 @@ int main() {
     consoleInstance.Write("Are you ready to start the adventure?").ThenInsertNewLine(1);
     std::cin >> input;
     if (ToLower(input) == "y" || ToLower(input) == "yy" || ToLower(input) == "yes") {
-        consoleInstance.Write("Let's begin. Oh wait, we just did! One of the first things I learned was how to create a function that turns a text into a lowercase")
+        consoleInstance.Write("Let's begin... One of the first things I learned was how to create a function that turns a text into a lowercase")
             .ThenInsertNewLine(1)
             .ThenWaitFor(800)
             .Write("Try and input something into the console and see how it magically turns into all lowercase:")
@@ -72,7 +72,7 @@ int main() {
             .ThenWaitFor(800)
             .Write("/* Multiline comment */")
             .ThenInsertNewLine(2)
-            .ThenWaitFor(2000)
+            .ThenWaitFor(8000)
             .Clear();
 
         consoleInstance.Write("Every program starts with a 'Hello World'!")
@@ -80,7 +80,7 @@ int main() {
             .ThenWaitFor(800)
             .Write("In C++, to output something to the console, we use 'std::cout << '(your text)', we use the insertion operator for this.")
             .ThenInsertNewLine(2)
-            .ThenWaitFor(2000)
+            .ThenWaitFor(8000)
             .Clear();
 
         consoleInstance.Write("Let's have a look at the type system...")
@@ -93,13 +93,13 @@ int main() {
             .ThenWaitFor(800)
             .Write("'void' is a special type used with pointers and functions.")
             .ThenInsertNewLine(2)
-            .ThenWaitFor(2000)
+            .ThenWaitFor(8000)
             .Clear();
 
         consoleInstance.Write("C++ also provides modifiers that help modify the meaning of the type, for example signed, unsigned, short, long.")
                 .ThenInsertNewLine(1)
                 .ThenWaitFor(800)
-                .Write("Signed and unsigned can only be used on integrals – not on floating point types.")
+                .Write("Signed and unsigned can only be used on integrals, not on floating point types.")
                 .ThenInsertNewLine(1)
                 .ThenWaitFor(800)
                 .Write("All types can be qualified with qualifiers, for example const, volatile, static.")
@@ -110,9 +110,27 @@ int main() {
                 .ThenWaitFor(800)
                 .Write("On a 16-bit platform, an integer may require 16 bits of memory, while on a 32 bit it will require 32 bits of memory.")
                 .ThenInsertNewLine(2)
-                .ThenWaitFor(2000)
+                .ThenWaitFor(8000)
                 .Clear();
 
+        consoleInstance.Write("Basic_ostream is responsible for output and basic_istream for input.")
+                .ThenInsertNewLine(1)
+                .ThenWaitFor(800)
+                .Write( "C++ provides ostream and istream definitions for convenience.")
+                .ThenInsertNewLine(1)
+                .ThenWaitFor(800)
+                .Write("Cout is an object of ostream class and cin is an object of istream class.")
+                .ThenInsertNewLine(1)
+                .ThenWaitFor(800)
+                .Write( "Cout represents the console, and to write anything to the console we have to insert it into the cout object using the << operator.")
+                .ThenInsertNewLine(1)
+                .ThenWaitFor(800)
+                .Write("To read from the keyboard we use the cin object, and we extract the characters from the cin object with the extraction the >> operator.")
+                .ThenInsertNewLine(2)
+                .Write("The objects and classes are declared in the iostream header.")
+                .ThenInsertNewLine(2)
+                .ThenWaitFor(8000)
+                .Clear();
     } else {
         consoleInstance.Write("That's a shame...");
     }
