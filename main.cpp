a3b5385816013625c75e3f821e1db7f5df237f53
xTheCoderx <justaguyoney@gmail.com>
2021-10-18 16:04:17 +1300

    Added some more code, if the user says that they want to star the adventure, the adventure will begin.

--- main.cpp
+++ main.cpp
@@ -14,5 +14,11 @@ int main() {
 
     std::cout << "Are you ready to start the adventure?" << "\n";
 
+    std::cin >> input;
+
+    if (ToLower(input) == "y" || ToLower(input) == "yy" || ToLower(input) == "yes") {
+        std::cout << "Let's begin. Oh wait, we just did! One of the first things I learned was how to create a function that turns a text into a lowercase" << "\n";
+        std::cout << "Try and input something into the console and see how it magically turns into all lowercase:" << "\n";
+    }
     return 0;
 }
