32988fccf32f2def888d7c8151965c2321b34955
xTheCoderx <justaguyoney@gmail.com>
2021-10-18 16:19:02 +1300

    The user now will get their message printed out to lower case.

--- main.cpp
+++ main.cpp
@@ -19,6 +19,13 @@ int main() {
     if (ToLower(input) == "y" || ToLower(input) == "yy" || ToLower(input) == "yes") {
         std::cout << "Let's begin. Oh wait, we just did! One of the first things I learned was how to create a function that turns a text into a lowercase" << "\n";
         std::cout << "Try and input something into the console and see how it magically turns into all lowercase:" << "\n";
+
+        std::string input_two;
+
+        std::cin >> input;
+
+        std::cout << ToLower(input);
+
     } else {
         std::cout << "That's a shame." << "\n";
     }
